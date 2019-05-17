71bcdc1abba88e4c48e4ac8efc9ed6f686ae7218
krzyczak <krzyczak@users.noreply.github.com>
2019-05-17 18:50:20 +0100

    hgd

--- gilded_rose.py
+++ gilded_rose.py
@@ -20,6 +20,7 @@ class Brie:
         if self.sell_in < 0:
             if self.quality < 50:
                 self.quality = self.quality + 1
+
             if self.sell_in <= 0:
                 self.quality = 0
 
@@ -34,6 +35,7 @@ class Backstage:
 
     def tick(self):
         self.sell_in = self.sell_in - 1
+        if self.quality >= 50: return
 
         if self.quality < 50:
             self.quality = self.quality + 1
@@ -46,38 +48,30 @@ class Backstage:
                 if self.quality < 50:
                     self.quality = self.quality + 1
 
-        if self.sell_in < 0: self.quality = 0
+        if self.sell_in < 0:
+            self.quality = 0
 
-        if self.quality > 50:
-            self.quality = 50
-
-class Sulfuras:
+class Conjured:
     def __init__(self, sell_in, quality):
         self.sell_in = sell_in
         self.quality = quality
-        self.name = "Sulfuras, Hand of Ragnaros"
+        self.name = "Conjured"
 
     def tick(self):
-        pass
+        self.sell_in = self.sell_in - 1
 
-# class Vest:
-#     def __init__(self, sell_in, quality):
-#         self.sell_in = sell_in
-#         self.quality = quality
-#         self.name = "+5 Dexterity Vest"
+        self.quality = self.quality - 1
 
-#     def tick(self):
-#         self.sell_in = self.sell_in - 1
-
-#         if self.quality > 0:
-#             self.quality = self.quality - 1
-
-#         if self.sell_in < 0:
-#             if self.quality > 0:
-#                 self.quality = self.quality - 1
+        if self.sell_in < 0:
+            if self.quality > 0:
+                self.quality = self.quality - 1
 
 
 class GildedRose:
+    # "Backstage passes to a TAFKAL80ETC concert" => Backstage,
+    # "Aged Brie" => Brie,
+    # "Sulfuras, Hand of Ragnaros" => Item,
+    # "Conjured Mana Cake" => Conjured
     @staticmethod
     def create_item(name, sell_in, quality):
         if name == "Aged Brie":
@@ -87,13 +81,13 @@ class GildedRose:
             return Backstage(sell_in, quality)
 
         if name == "Sulfuras, Hand of Ragnaros":
-            return Sulfuras(sell_in, quality)
+            return Item("Sulfuras, Hand of Ragnaros", sell_in, quality)
+
+        if name == "Conjured Mana Cake":
+            return Conjured(sell_in, quality)
 
         # if name == "+5 Dexterity Vest":
-        #     return Vest(sell_in, quality)
-
-        print(f"returning default for {name}")
-
+        #     return Conjured(sell_in, quality)
         return Item(name, sell_in, quality)
 
     @staticmethod
--- item.py
+++ item.py
@@ -5,47 +5,4 @@ class Item:
         self.quality = quality
 
     def tick(self):
-        if "Aged Brie" != self.name and "Backstage passes to a TAFKAL80ETC concert" != self.name:
-            # TODO: Improve this code.  Word.
-            if self.quality > 0:
-                if "Sulfuras, Hand of Ragnaros" != self.name:
-                    self.quality = self.quality - 1
-        else:
-            if self.quality < 50:
-                self.quality = self.quality + 1
-                if "Aged Brie" == self.name:
-                    if self.sell_in < 6:
-                        self.quality = self.quality + 1
-                # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                if "Aged Brie" == self.name:
-                    if self.sell_in < 11:
-                        self.quality = self.quality + 1
-                if "Backstage passes to a TAFKAL80ETC concert" == self.name:
-                    if self.sell_in < 11:
-                        # See revision number 2394 on SVN.
-                        if self.quality < 50:
-                            self.quality = self.quality + 1
-                    # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                    if self.sell_in < 6:
-                        if self.quality < 50:
-                            self.quality = self.quality + 1
-        if "Sulfuras, Hand of Ragnaros" != self.name:
-            self.sell_in = self.sell_in - 1
-        if self.sell_in < 0:
-            if "Aged Brie" != self.name:
-                if "Backstage passes to a TAFKAL80ETC concert" != self.name:
-                    if self.quality > 0:
-                        if "Sulfuras, Hand of Ragnaros" != self.name:
-                            self.quality = self.quality - 1
-                else:
-                    # TODO: Fix this.
-                    self.quality = self.quality - self.quality
-            else:
-                if self.quality < 50:
-                    self.quality = self.quality + 1
-                if "Aged Brie" == self.name and self.sell_in <= 0:
-                    self.quality = 0
-                    # of for.
-        if "Sulfuras, Hand of Ragnaros" != self.name:
-            if self.quality > 50:
-                self.quality = 50
+        pass
